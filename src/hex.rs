09661273f02e982b3e95e5cd022b3b25ba8b5dca
bcorrigan <b.j.corrigan@gmail.com>
2024-04-17 21:18:57 +0100

    Update deps

--- src/hex.rs
+++ src/hex.rs
@@ -1,15 +1,15 @@
-use hexgrid::{Coordinate, Spacing,Spin, Direction};
-use rand::{Rng, SeedableRng};
+use hexgrid::{Coordinate, Direction, Spacing, Spin};
 use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng};
+use std::cmp::{max, min};
 use std::collections::HashMap;
-use std::cmp::{max,min};
 
 pub struct World {
     pub map: HashMap<Coordinate, Type>,
     pub edge_lookup: HashMap<Coordinate, Coordinate>,
     pub size: f32,
     pub spacing: Spacing,
-    pub radius:i32,
+    pub radius: i32,
     pub rule: Rule,
 }
 
@@ -17,49 +17,47 @@ pub struct Rule {
     pub survival: Vec<u8>,
     pub birth: Vec<u8>,
     pub states: u8,
-
 }
 
 impl World {
     pub fn new() -> Self {
         let mut map = HashMap::default();
         let mut small_rng = SmallRng::from_entropy();
-        let map_radius:i32 =48;
+        let map_radius: i32 = 80;
 
         let rule = Rule {
-            survival: vec![3,1,0],
-            birth: vec![3],
+            survival: vec![2, 1],
+            birth: vec![2],
             states: 3,
         };
 
-        //coordinates of 6 mirrored hexagonal origins 
+        //coordinates of 6 mirrored hexagonal origins
         let mirror_coords = [
-            Coordinate::from_cubic(-map_radius, -map_radius-1, 2*map_radius+1),
-            Coordinate::from_cubic(map_radius+1, -2*map_radius-1, map_radius),
-            Coordinate::from_cubic(2*map_radius+1, -map_radius, -map_radius-1),
-            Coordinate::from_cubic(map_radius, map_radius+1, -2*map_radius-1),
-            Coordinate::from_cubic(-map_radius-1, 2*map_radius+1, -map_radius),
-            Coordinate::from_cubic(-2*map_radius-1, map_radius, map_radius+1),
+            Coordinate::from_cubic(-map_radius, -map_radius - 1, 2 * map_radius + 1),
+            Coordinate::from_cubic(map_radius + 1, -2 * map_radius - 1, map_radius),
+            Coordinate::from_cubic(2 * map_radius + 1, -map_radius, -map_radius - 1),
+            Coordinate::from_cubic(map_radius, map_radius + 1, -2 * map_radius - 1),
+            Coordinate::from_cubic(-map_radius - 1, 2 * map_radius + 1, -map_radius),
+            Coordinate::from_cubic(-2 * map_radius - 1, map_radius, map_radius + 1),
         ];
 
         for q in -map_radius..=map_radius {
             let r1 = max(-map_radius, -q - map_radius);
             let r2 = min(map_radius, -q + map_radius);
             for r in r1..=r2 {
-                let coordinate = Coordinate::from_cubic(q,r, 0 - q - r);
+                let coordinate = Coordinate::from_cubic(q, r, 0 - q - r);
                 let data = if small_rng.gen_bool(0.5) {
-                    Type::On(rule.states-1)
+                    Type::On(rule.states - 1)
                 } else {
                     Type::Off
                 };
                 map.insert(coordinate, data);
-                
             }
         }
 
-        let mut edge_lookup:HashMap<Coordinate,Coordinate> = HashMap::default();
-        let origin = Coordinate::from_cubic(0,0,0);
-        for ring_c in origin.ring_iter(map_radius+1, Spin::CCW(Direction::XY)) {
+        let mut edge_lookup: HashMap<Coordinate, Coordinate> = HashMap::default();
+        let origin = Coordinate::from_cubic(0, 0, 0);
+        for ring_c in origin.ring_iter(map_radius + 1, Spin::CCW(Direction::XY)) {
             for mirror in mirror_coords {
                 if ring_c.distance(mirror) <= map_radius {
                     edge_lookup.insert(ring_c, ring_c - mirror);
@@ -67,66 +65,71 @@ impl World {
                 }
             }
         }
- 
-        let size=7.0;
 
-        
-        World { 
+        let size = 7.0;
+
+        World {
             map,
             edge_lookup,
             size,
             spacing: Spacing::FlatTop(size),
-            radius:map_radius,
+            radius: map_radius,
             rule,
         }
     }
 
     pub fn iterate(&mut self) {
-        let mut map:HashMap<Coordinate,Type> = HashMap::default();
+        let mut map: HashMap<Coordinate, Type> = HashMap::default();
 
         for coord in self.map.keys() {
             let mut raw_nbrs = coord.neighbors();
-            let neighbours = raw_nbrs.iter_mut().map(|coord| {
-                if self.edge_lookup.contains_key(coord) {
-                    self.edge_lookup.get(coord).unwrap()
-                } else {
-                    coord
-                }
-            } ).collect::<Vec<_>>();
+            let neighbours = raw_nbrs
+                .iter_mut()
+                .map(|coord| {
+                    if self.edge_lookup.contains_key(coord) {
+                        self.edge_lookup.get(coord).unwrap()
+                    } else {
+                        coord
+                    }
+                })
+                .collect::<Vec<_>>();
 
-            let alive_count:u8 = neighbours.iter().fold(0, |acc, coord| {
-                match self.map.get(*coord).unwrap() {
-                    Type::On(_) => acc+1,
-                    Type::Off => acc,
-                }
-            });
+            let alive_count: u8 =
+                neighbours
+                    .iter()
+                    .fold(0, |acc, coord| match self.map.get(*coord).unwrap() {
+                        Type::On(_) => acc + 1,
+                        Type::Off => acc,
+                    });
 
             match self.map.get(coord).unwrap() {
                 Type::On(s) => {
                     //already On, so we check if it survives
                     if self.rule.survival.contains(&alive_count) {
                         //do we reset to state 5 here?? or just increment existing?
-                        let init_s = if s<&(self.rule.states-1) {
-                            s+1
-                        } else { *s };
+                        let init_s = if s < &(self.rule.states - 1) {
+                            s + 1
+                        } else {
+                            *s
+                        };
                         map.insert(*coord, Type::On(init_s));
                     } else {
                         //die = decrement counter
-                        let new_s = s-1;
-                        if new_s==0 {
+                        let new_s = s - 1;
+                        if new_s == 0 {
                             //Off
                             map.insert(*coord, Type::Off);
                         } else {
                             map.insert(*coord, Type::On(new_s));
                         }
                     }
-                },
+                }
                 Type::Off => {
                     if self.rule.birth.contains(&alive_count) {
-                        //does this init at s=5 or s=1? 
+                        //does this init at s=5 or s=1?
                         map.insert(*coord, Type::On(1));
                     }
-                },
+                }
             }
         }
 
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use ggez::event;
 use ggez::graphics;
-use ggez::graphics::{MeshBatch, DrawMode, FillOptions};
+use ggez::graphics::{Color, Mesh};
+use ggez::graphics::{DrawMode, FillOptions, InstanceArray};
 use ggez::{Context, GameResult};
+use glam::Vec2;
 use hex::World;
 use std::env;
-use std::path;
-use glam::Vec2;
 use std::f32::consts::PI;
+use std::path;
 
 mod hex;
 
 // First we make a structure to contain the game's state
 struct MainState {
     frames: usize,
-    meshbatch: MeshBatch,
+    meshbatch: InstanceArray,
+    mesh: Mesh,
     world: World,
 }
 
@@ -26,63 +28,83 @@ impl MainState {
         //size is edge length
         //coordinates are from simple trig
 
-        let h = world.size * (PI/3.0).sin();
-        let t = world.size * (PI/6.0).sin();
+        let h = world.size * (PI / 3.0).sin();
+        let t = world.size * (PI / 6.0).sin();
         let margin = 1.0;
 
         mb.polygon(
-           DrawMode::Fill(FillOptions::default()),
-         &[
-            Vec2::new(0.0+margin,0.0-margin),
-            Vec2::new(world.size-margin, 0.0-margin),
-            Vec2::new(world.size + t-margin, -h),
-            Vec2::new(world.size-margin, -2.0 * h + margin),
-            Vec2::new(0.0+margin, -2.0 * h + margin),
-            Vec2::new(-t+margin, -h),
-            Vec2::new(0.0+margin,0.0-margin),
-          ],
-          graphics::Color::new(1.0, 1.0, 1.0, 1.0))?;
+            DrawMode::Fill(FillOptions::default()),
+            &[
+                Vec2::new(0.0 + margin, 0.0 - margin),
+                Vec2::new(world.size - margin, 0.0 - margin),
+                Vec2::new(world.size + t - margin, -h),
+                Vec2::new(world.size - margin, -2.0 * h + margin),
+                Vec2::new(0.0 + margin, -2.0 * h + margin),
+                Vec2::new(-t + margin, -h),
+                Vec2::new(0.0 + margin, 0.0 - margin),
+            ],
+            graphics::Color::new(1.0, 1.0, 1.0, 1.0),
+        )?;
 
-        let meshbatch = MeshBatch::new(mb.build(ctx)?)?;
-        let s = MainState { frames: 0, meshbatch, world };
+        let meshbatch = InstanceArray::new(ctx, None);
+
+        //let instancearray = InstanceArray::new(&mb.build(ctx), None);
+        let s = MainState {
+            frames: 0,
+            meshbatch,
+            mesh: Mesh::from_data(ctx, mb.build()),
+            world,
+        };
         Ok(s)
     }
 }
 
-impl event::EventHandler<ggez::GameError> for MainState {
+impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
         self.meshbatch.clear();
 
         //if (self.frames % 25) == 24 {
-			self.world.iterate();
+        self.world.iterate();
         //}
-
 
         //todo only clear meshbatch and recalculate world if something changes
         for (cell, data) in &self.world.map {
-            let (x,y) = cell.cartesian_center(self.world.spacing);
-            let p = graphics::DrawParam::new().dest(
-                Vec2::new(x+2.0 * self.world.radius as f32*self.world.size,y+2.0*self.world.radius as f32*self.world.size));
+            let (x, y) = cell.cartesian_center(self.world.spacing);
+            let p = graphics::DrawParam::new().dest(Vec2::new(
+                x + 2.0 * self.world.radius as f32 * self.world.size,
+                y + 2.0 * self.world.radius as f32 * self.world.size,
+            ));
             let p2 = match data {
-                &hex::Type::On(i) => if i==2 {
-                    //
-                    p.color(graphics::Color::new(1.0, 1.0, 0.0, 1.0))
-                } else { p.color(graphics::Color::new(0.8, 0.0, 0.0, 1.0)) }, //pink
+                &hex::Type::On(i) => {
+                    if i == 2 {
+                        //
+                        p.color(graphics::Color::new(1.0, 1.0, 0.0, 1.0))
+                    } else {
+                        p.color(graphics::Color::new(0.8, 0.0, 0.0, 1.0))
+                    }
+                } //pink
                 &hex::Type::Off => p.color(graphics::Color::new(0.2, 0.2, 0.2, 1.0)), //yellow
             };
 
-            self.meshbatch.add(p2);
+            self.meshbatch.push(p2);
         }
 
-        self.meshbatch.draw(ctx, graphics::DrawParam::default())?;
+        //self.meshbatch.draw(ctx, graphics::DrawParam::default())?;
+        //canvas.draw(&self.meshbatch, graphics::DrawParam::default());
 
-        graphics::present(ctx)?;
+        canvas.draw_instanced_mesh(
+            self.mesh.clone(),
+            &self.meshbatch,
+            graphics::DrawParam::default(),
+        );
 
+        //graphics::present(ctx)?;
+        canvas.finish(ctx)?;
         self.frames += 1;
         if (self.frames % 100) == 0 {
             println!("FPS: {}", ggez::timer::fps(ctx));
@@ -101,8 +123,16 @@ pub fn main() -> GameResult {
         path::PathBuf::from("./resources")
     };
 
-    let cb = ggez::ContextBuilder::new("hexxxx", "bcorrigan").add_resource_path(resource_dir)
-    .window_mode(ggez::conf::WindowMode::default().dimensions(1400.0, 1300.0));
+    let mut wm = ggez::conf::WindowMode::default()
+        .dimensions(1920.0, 1200.0)
+        //.fullscreen_type(ggez::conf::FullscreenType::True)
+        .resizable(true)
+        .resize_on_scale_factor_change(true);
+    wm.logical_size = None;
+
+    let cb = ggez::ContextBuilder::new("hexxxx", "bcorrigan")
+        .add_resource_path(resource_dir)
+        .window_mode(wm);
     let (mut ctx, event_loop) = cb.build()?;
     graphics::set_window_title(&ctx, "Hexagons are the bestagons");
 
